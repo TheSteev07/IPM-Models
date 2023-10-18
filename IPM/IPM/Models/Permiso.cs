89793b673c0a195a316338c195ec3453d0f3e098
TheSteev07 <steevrom29@gmail.com>
2023-10-18 12:29:56 -0500

    prueba

---
+++ IPM/IPM/Models/Permiso.cs
@@ -0,0 +1,11 @@
+﻿namespace IPM.Models
+{
+    public class Permiso
+    {
+        public int PermisoId { get; set; }
+        public string? Nombre { get; set; }
+
+        // Relación con Roles a través de la tabla de asignación RolPermiso
+        public ICollection<RolPermiso>? RolesPermisos { get; set; }
+    }
+}
---
+++ IPM/IPM/Models/Rol.cs
@@ -0,0 +1,14 @@
+﻿namespace IPM.Models
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string? Nombre { get; set; }
+
+        // Relación con Usuarios
+        public ICollection<Usuarios>? Usuarios { get; set; }
+
+        // Relación con Permisos a través de la tabla de asignación RolPermiso
+        public ICollection<RolPermiso>? RolesPermisos { get; set; }
+    }
+}
